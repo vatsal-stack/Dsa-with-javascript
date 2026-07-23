1f0bb41a15eb94223a2782904c9c6012356beb0c
vatsal-stack <iamvatsalpanchal@gmail.com>
2026-07-23 22:05:47 +0530

    code

---
+++ scrip.js
@@ -0,0 +1,20 @@
+function solve(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (max > arr[i]) {
+      max = arr[i];
+    }
+  }
+  console.log(max);
+  
+}
+
+solve([4, 7, 1, 2, 9, 8]);
+
+
+    if (max > arr[i]) {
+      max = arr[i];
+    }
+  }
+  console.log(max);
